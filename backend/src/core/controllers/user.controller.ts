e350b8206b790502be2d597bd903b65e131861a5
tilak-dev <tsingh99991@gmail.com>
2025-01-29 18:40:11 +0530

    userResetPasswordRequest added

--- backend/src/core/controllers/user.controller.ts
+++ backend/src/core/controllers/user.controller.ts
@@ -1,8 +1,13 @@
 import appAssert from "../../common/API/AppAssert";
+import { emailSchema } from "../../common/schemas/auth";
 import { BAD_REQUEST, OK } from "../../constants/http";
+import User from "../../database/models/user.model";
 import asyncHandler from "../../middlewares/asyncHandler.middleware";
 import { validateFileImage } from "../../middlewares/file.middleware";
-import { userAvatarService } from "../services/user.service";
+import {
+  userAvatarService,
+  userPasswordResetRequestService,
+} from "../services/user.service";
 
 export const userProfileImageHandle = asyncHandler(async (req, res) => {
   const userId = req.userId;
@@ -19,3 +24,14 @@ export const userProfileImageHandle = asyncHandler(async (req, res) => {
     user,
   });
 });
+
+export const userResetPasswordRequest = asyncHandler(async (req, res) => {
+  const email = emailSchema.parse(req.body.email);
+
+  await userPasswordResetRequestService({ email });
+
+  return res.status(OK).json({
+    message: "Password reset email sent successfully ",
+  });
+});
+
--- backend/src/core/services/user.service.ts
+++ backend/src/core/services/user.service.ts
@@ -3,6 +3,8 @@ import User from "../../database/models/user.model";
 import appAssert from "../../common/API/AppAssert";
 import { BAD_REQUEST } from "../../constants/http";
 import uploadFileToCloudinary from "../../common/utils/cloudinary";
+import VerifyCation from "../../database/models/vaerifiacation.model";
+import { verificationCode } from "../../common/enum/verificationCode";
 
 type UserAvatar = {
   avatar: string;
@@ -20,6 +22,23 @@ export const userAvatarService = async (data: UserAvatar) => {
   return { user };
 };
 
+type UserPasswordResetRequestType = {
+  email: string;
+};
+
+export const userPasswordResetRequestService = async (
+  data: UserPasswordResetRequestType
+) => {
+  const user = await User.findOne({ email: data.email });
+  appAssert(user, BAD_REQUEST, "user not found");
+
+  const passwordResetVerificationCode = await VerifyCation.create({
+    userId: user._id,
+    type: verificationCode.PASSWORD_RESET,
+  });
+  // TODO sent email verification
+  return { passwordResetVerificationCode };
+};
 
 type UserPasswordChangeServiceType = {
   userId: string;
